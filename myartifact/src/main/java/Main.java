1e3e3eb41c678aa805193c1a5c24fd0f897d2d3f
Ysepchuck <esepchuck.dmitrii@gmail.com>
2018-06-26 15:22:23 +0300

    add new feature

--- myartifact/src/main/java/Main.java
+++ myartifact/src/main/java/Main.java
@@ -1,7 +1,20 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println( "Hello ");
-    };
+    public static void main(String[] args) {
+        hello("World");
+
+
+        double l = 6;
+        System.out.println(" площядь квадрата " + l + " = " + area (l));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println( "Hello " + somebody + "!");
+
+    }
+    public static double area (double len) {
+        return len * len;
+    }
+
 }
